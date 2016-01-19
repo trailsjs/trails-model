3cf95e6ddab5ce7291b9c61fed01dbd816e7705c
jaumard <ioquoo3H>
2016-01-19 11:07:50 +0100

    Add description method and some comments

--- index.js
+++ index.js
@@ -8,11 +8,27 @@ module.exports = class TrailsModel {
     this.app = app
   }
 
+  /**
+   * Specific model configuration (override config/models.js)
+   * @return object config to merge with defaults
+   */
   static config () {
 
   }
 
+  /**
+   * Model schema in ORM specific structure (waterline, Bookshelf...)
+   * @return object schema
+   */
   static schema () {
+
+  }
+
+  /**
+   * Model description, can be use for documentation generator like swagger
+   * @return object
+   */
+  static description () {
 
   }
 
